2a987a4eb508476573ef88262ebefa2a4fa8d71a
Ace Baum <jaybaum.03@gmail.com>
2024-01-04 12:27:07 -0500

    queue

--- TS/sort/LinkedList.ts
+++ TS/sort/LinkedList.ts
@@ -8,29 +8,11 @@ interface LinkedList<T> {
     get(index: number): T | undefined;
 }
 
-class ListNode<T> {
-    private value: T;
-    public next: ListNode<T> | null;
-
-    constructor(value: T, next: ListNode<T> | null) {
-        this.value = value;
-        this.next = next;
-    }
-
-    getValue(): T {
-        return this.value;
-    }
-}
-
 class LinkedList1<T> implements LinkedList<T> {
     public length: number;
-    private head: ListNode<T> | null;
-    private tail: ListNode<T> | null;
 
     constructor() {
         this.length = 0;
-        this.head = null;
-        this.tail = null;
     }
 
     insertAt(item: T, index: number): void {}
